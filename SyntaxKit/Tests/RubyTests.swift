92439a00af7a3b3527c09c19310c4f36bb9d7bf5
aaronvegh <aaron@innoveghtive.com>
2016-05-27 19:05:48 -0400

    Add ruby test

---
+++ SyntaxKit/Tests/RubyTests.swift
@@ -0,0 +1,70 @@
+//
+//  RubyTest.swift
+//  SyntaxKit
+//
+//  Created by Aaron Vegh on 2016-05-26.
+//  Copyright © 2016 Sam Soffes. All rights reserved.
+//
+
+import XCTest
+@testable import SyntaxKit
+
+class RubyTests: XCTestCase {
+
+    // MARK: - Properties
+
+    let parser = Parser(language: language("Ruby"))
+
+    func testRubyParsing() {
+        let input = fixture("view.rb", "txt")
+        parser.parse(input) { scope, range in
+            print("Scope: \(scope), String: \(input.substringWithRange(range))")
+        }
+    }
+
+
+    // MARK: - Tests
+
+    func testParsingBeginEnd() {
+        var stringQuoted: NSRange?
+        var punctuationBegin: NSRange?
+        var punctuationEnd: NSRange?
+
+        parser.parse("title: \"Hello World\"\n") { scope, range in
+            if stringQuoted == nil && scope.hasPrefix("string.quoted.double") {
+                stringQuoted = range
+            }
+
+            if punctuationBegin == nil && scope.hasPrefix("punctuation.definition.string.begin") {
+                punctuationBegin = range
+            }
+
+            if punctuationEnd == nil && scope.hasPrefix("punctuation.definition.string.end") {
+                punctuationEnd = range
+            }
+        }
+
+        XCTAssertEqual(NSMakeRange(7, 13), stringQuoted!)
+        XCTAssertEqual(NSMakeRange(7, 1), punctuationBegin!)
+        XCTAssertEqual(NSMakeRange(19, 1), punctuationEnd!)
+    }
+
+    func testParsingBeginEndCrap() {
+        var stringQuoted: NSRange?
+
+        parser.parse("title: Hello World\ncomments: 24\nposts: \"12\"zz\n") { scope, range in
+            if stringQuoted == nil && scope.hasPrefix("string.quoted.double") {
+                stringQuoted = range
+            }
+        }
+        
+        XCTAssertEqual(NSMakeRange(39, 4), stringQuoted!)
+    }
+    
+    func testRuby() {
+        let parser = Parser(language: language("Ruby"))
+        let input = fixture("test.rb", "txt")
+        parser.parse(input, match: { _, _ in return })
+    }
+}
+
